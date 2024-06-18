3337955f3655a973ff69bbc1f5f60ebc3c3be557
YUERONG HUANG <rowanne.h@hotmail.com>
2024-06-18 23:58:49 +1000

    complete lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,10 @@
 # your code goes here!
+import re
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses=email_addresses
+
+    def parse(self):
+        pattern=r'[a-z]+\.?[a-z]+@[a-z]+\.com'
+        emailRegex=re.compile(pattern)
+        return sorted(emailRegex.findall(self.email_addresses))
